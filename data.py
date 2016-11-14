4fb6703359f8870fd2c10db48d34f608c823203b
pitlv2109 <pitlv2109@gmail.com>
2016-11-13 22:53:04 -0600

    review page

--- data.py
+++ data.py
@@ -1,12 +1,19 @@
 from flask_wtf import Form 
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.widgets import TextArea
+from wtforms.validators import DataRequired
 
 
 class SearchBar(Form):
-	course_name = StringField('Enter a course...')
+	course_name = StringField('Enter a course...', validators=[DataRequired()])
 	submit = SubmitField('Search')
 
 class CourseData(Form):
-	review = StringField('Write a review...')
-	tip = StringField('Write a tip...')
+	review = StringField(widget=TextArea(), validators=[DataRequired()])
+	hours = SelectField(
+        'Number of hours',
+        choices=[('question', 'How many hours did you have to work for this class?'), 
+        ('below-3', 'Below 3'), ('3to6', '3-6'), ('7to10', '7-10'), ('11to14', '11 to 14'),
+        ('15to18', '15 to 18'), ('nolife', "I didn't have a life")], validators=[DataRequired()]
+    )
 	submit = SubmitField('Submit')
